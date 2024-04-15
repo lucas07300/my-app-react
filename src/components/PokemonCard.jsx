95ea219d347e4cc528b0fac98fd49501c88bd85f
lucas07300 <161328245+lucas07300@users.noreply.github.com>
2024-04-15 09:32:25 +0200

    ajout du composant 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -5,6 +5,26 @@ const PokemonCard = () => {
       <figcaption>Bulbasaur</figcaption>
     </figure>
   );
+
+const pokemonList = [
+      {name: "bulbasaur", imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"},
+,
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = pokemonList[1];
+
+  return (
+    <figure>
+    {pokemon.imgSrc ? (
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+    ) : (
+      <p>???</p>
+    )}
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>
+);
 }
 
 export default PokemonCard;
